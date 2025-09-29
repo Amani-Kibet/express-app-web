f759f0111bf7d20f8411500f9fdbfafea584495a
Amani Kibet <amanikbt1@gmail.com>
2025-09-29 09:12:45 +0300

    commit

--- .history/index_20250924210012.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.get()
--- .history/index_20250927155147.js
+++
@@ -1,2 +0,0 @@
-cd "C:\Users\USER\Desktop\React Projects\express-app-web"
-git status
